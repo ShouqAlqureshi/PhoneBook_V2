8c69c32e8acc7ea65f7a38c82abfc9b368ff6d76
ShouqAlqureshi <123020118+ShouqAlqureshi@users.noreply.github.com>
2023-10-31 11:40:17 +0300

    upload contact, events initial version

---
+++ src/Contact.java
@@ -0,0 +1,84 @@
+public class Contact implements Comparable<Contact> {
+
+    private String Name, Phone_Number, Email_Address, Birthday, Address;
+
+    String note;
+
+//    EventLinkedList scheduledEvents = new EventLinkedList();
+
+    public Contact(String name, String phone_Number, String email_Address, String birthday, String address, String note) {
+        setName(name);
+        setPhone_Number(phone_Number);
+        setEmail_Address(email_Address);
+        setBirthday(birthday);
+        setAddress(address);
+        this.note = note;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public void setPhone_Number(String phone_Number) {
+        Phone_Number = phone_Number;
+    }
+
+    public void setEmail_Address(String email_Address) {
+        Email_Address = email_Address;
+    }
+
+    public void setBirthday(String birthday) {
+        Birthday = birthday;
+    }
+
+    public void setAddress(String address) {
+        Address = address;
+    }
+
+    public String getAddress() {
+        return Address;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public String getPhone_Number() {
+        return Phone_Number;
+    }
+
+    public String getEmail_Address() {
+        return Email_Address;
+    }
+
+    public String getBirthday() {
+        return Birthday;
+    }
+
+    @Override
+    public int compareTo(Contact contact) {
+        if (this.Name.toUpperCase().charAt(0) > contact.getName().toUpperCase().charAt(0))
+            return 1;
+        else if (this.Name.toUpperCase().charAt(0) == contact.getName().toUpperCase().charAt(0)) {
+            int limit;
+            if (!this.Name.equalsIgnoreCase(contact.getName())) {
+                limit = Math.min(this.Name.length(), contact.getName().length());
+                for (int i = 1; i < limit; i++) {
+                    if (this.Name.toUpperCase().charAt(i) > contact.getName().toUpperCase().charAt(i)) {
+                        return 1;
+                    } else if (this.Name.toUpperCase().charAt(i) < contact.getName().toUpperCase().charAt(i)) {
+                        return -1;
+                    }
+                }
+                if (this.Name.length() != contact.getName().length())
+                    return 2;
+            }
+            return 0;
+        } else
+            return -1;
+    }
+
+    public String toString() {
+        return getClass().getName() + "name: " + getName() + "\tPhone Number:" + getPhone_Number() + "\tAddress: " + getAddress() + "\nEmail_Address: " + getEmail_Address() + "\tBirthday: " + getBirthday() + "\n note: " + note + "";
+    }
+}//class
---
+++ src/Event.java
@@ -0,0 +1,89 @@
+public class Event  {
+
+    String title;
+
+    String date;
+
+    String time;
+
+    String location;
+
+    Contact contact ;
+
+    public Event() {
+        this.title = "";
+        this.date = null;
+        this.time = "";
+        this.location = "";
+    }
+
+    public Event(String title, String date, String time, String location, Contact contact) {
+        this.title = title;
+        this.date = date;
+        this.time = time;
+        this.location = location;
+        this.contact = contact ;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public String getContact() {
+        return contact.toString();
+    }
+    @Override
+    public String toString() {
+        return "Event{" + "title=" + title +", contact name="+contact.getName()+ ", date=" + date + ", time=" + time + ", location=" + location + '}';
+    }
+
+    public int comparing(Event event) {
+        if (this.title.toUpperCase().charAt(0) > event.title.toUpperCase().charAt(0))
+            return 1;
+        else if (this.title.toUpperCase().charAt(0) == event.title.toUpperCase().charAt(0)){
+            int limit;
+            if(!this.title.equalsIgnoreCase(event.title)) {
+                limit = Math.min(this.title.length(), event.title.length());
+                for (int i = 1; i < limit; i++) {
+                    if (this.title.toUpperCase().charAt(i) > event.title.toUpperCase().charAt(i)) {
+                        return 1;
+                    } else if (this.title.toUpperCase().charAt(i) < event.title.toUpperCase().charAt(i)) {
+                        return -1;
+                    }
+                }
+                if ( this.title.length() != event.title.length())
+                    return 2;
+            } return 0;
+        }else
+            return-1;
+    }
+
+}//class
